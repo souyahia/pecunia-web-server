0f0a8930ba2a00ace4395c44c7b3f0d8b0b503ef
Samy Ouyahia <ouyahia.samy@yahoo.fr>
2020-09-05 23:26:20 +0200

    added apidoc

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,30 +1,7 @@
 import { Request, Response } from 'express';
 
-/**
- * Catch requests on wrong end points.
- * @route ALL *
- */
 export function badEndPoint(req: Request, res: Response): void {
   res.status(400).send({
     message: `Cannot ${req.method} ${req.url}`,
   });
 }
-
-// /**
-//  * Catch requests on wrong end points.
-//  * @route ALL *
-//  */
-// export async function badEndPoint(req: Request, res: Response): Promise<void> {
-//   const repository = getManager().getRepository(User);
-//   const newUser = repository.create({
-//     email: 'bob@mail.com',
-//     id: 'testoooo-id',
-//     password: 'my-pass'
-//   });
-
-//   const created = await repository.save(newUser);
-//   res.status(400).send({
-//     message: `Cannot ${req.method} ${req.url}`,
-//     created
-//   });
-// }
--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,30 +1,26 @@
 import { Request, Response } from 'express';
-// import { getManager } from 'typeorm';
-// import { Keyword } from '../entities';
 
 /**
- * Ping the web server and get a greeting message.
- * @route GET /ping
+ * @api { get } /ping Ping the server.
+ * @apiName GetPing
+ * @apiGroup Ping
+ *
+ * @apiSuccess (200) { String } message Greeting message from the server.
+ * @apiSuccess (200) { String } date Current date time.
+ * @apiSuccess (200) { String } url Url of the request.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "Greetings from the Pecunia web server.",
+ *       "date": "2020-09-05T18:37:01.849Z",
+ *       "url": "/ping",
+ *     }
  */
-export function ping(req: Request, res: Response): void {
+export function getPing(req: Request, res: Response): void {
   res.status(200).send({
     message: 'Greetings from the Pecunia web server.',
     date: new Date().toISOString(),
     url: req.url,
   });
 }
-
-// /**
-//  * Ping the web server and get a greeting message.
-//  * @route GET /ping
-//  */
-// export async function ping(req: Request, res: Response): Promise<void> {
-//   const result = await getManager().getRepository(Keyword).find({ relations: [ 'category' ] });
-//   res.status(200).send({
-//     message: 'Greetings from the Pecunia web server.',
-//     date: new Date().toISOString(),
-//     url: req.url,
-//     result
-//   });
-// }
-
