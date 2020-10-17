f144ac63d8c0c21679d818a1e550b9cd5a455ef9
Samy Ouyahia <ouyahia.samy@yahoo.fr>
2020-10-17 18:30:26 +0200

    updated Auth controller test

--- test/controllers/auth.controller.test.ts
+++ test/controllers/auth.controller.test.ts
@@ -1,30 +1,17 @@
 import request from 'supertest';
 import { v4 } from 'uuid';
-import { getManager } from 'typeorm';
+import { getManager, Connection } from 'typeorm';
 import app from '../../src/app';
 import { User } from '../../src/entities';
-import { encryptPassword } from '../../src/auth/auth';
 import { connectDB, disconnectDB } from '../../src/database';
-import { UserRole } from '../../src/auth';
+import { UserRole, USER_ROLES, Auth } from '../../src/auth';
 import { clearDB } from '../testHelpers';
 
-const validEmail = 'test@mail.com';
-const validPassword = 'test-password';
+let connection: Connection = null;
 
 describe('Authentication controller', () => {
   beforeAll(async (done) => {
-    const connection = await connectDB();
-    await clearDB(connection);
-
-    const validUser = new User();
-    validUser.id = v4();
-    validUser.email = validEmail;
-    validUser.password = await encryptPassword(validPassword);
-    validUser.role = UserRole.User;
-    await validUser.validate();
-
-    const entityManager = getManager();
-    await entityManager.save(validUser);
+    connection = await connectDB();
     done();
   });
 
@@ -33,33 +20,102 @@ describe('Authentication controller', () => {
     done();
   });
 
-  it('POST /login should return 200 OK when given valid credentials', async (done) => {
-    const res = await request(app).post('/auth/login').send({
-      email: validEmail,
-      password: validPassword,
-    });
-    expect(res.status).toEqual(200);
+  beforeEach(async (done) => {
+    await clearDB(connection);
     done();
   });
 
-  it('POST /login should return the JWT token, the expire time and a message when given valid credentials', async (done) => {
-    const res = await request(app).post('/auth/login').send({
-      email: validEmail,
-      password: validPassword,
+  it('POST /signin should create a new User', async (done) => {
+    const newCredentials = {
+      email: 'my.new.user@mail.com',
+      password: 'test-password',
+    };
+
+    await request(app).post('/auth/signin').send({
+      email: newCredentials.email,
+      password: newCredentials.email,
     });
-    expect(typeof res.body.message).toBe('string');
+
+    const entityManager = getManager();
+    const lookupUser = await entityManager.findOne(User, {
+      where: {
+        email: newCredentials.email,
+      },
+    });
+
+    expect(lookupUser).toBeDefined();
+    done();
+  });
+
+  it('POST /signin should return 201 Created, the authorization details and the user informations', async (done) => {
+    const newCredentials = {
+      email: 'my.new.user@mail.com',
+      password: 'test-password',
+    };
+
+    const res = await request(app).post('/auth/signin').send({
+      email: newCredentials.email,
+      password: newCredentials.email,
+    });
+
+    expect(res.status).toEqual(201);
+    expect(typeof res.body.authorization.token).toBe('string');
+    expect(typeof res.body.authorization.expiresIn).toBe('string');
+    expect(res.body.user.email).toEqual(newCredentials.email);
+    expect(typeof res.body.user.id).toBe('string');
+    expect(typeof res.body.user.password).toBe('string');
+    expect(USER_ROLES).toContain(res.body.user.role);
+    done();
+  });
+
+  it('POST /signin should return 209 Conflict when given an email already taken', async (done) => {
+    const user = new User();
+    user.id = v4();
+    user.email = 'my.new.user@test.com';
+    user.password = await Auth.encryptPassword('test-password');
+    user.role = UserRole.Admin;
+
+    const entityManager = getManager();
+    await entityManager.save(user);
+
+    const res = await request(app).post('/auth/signin').send({
+      email: user.email,
+      password: 'test-password-test',
+    });
+
+    expect(res.status).toEqual(409);
+    done();
+  });
+
+  it('POST /login should return 200 OK with the authorization details when given valid credentials', async (done) => {
+    const user = new User();
+    user.id = v4();
+    user.email = 'my.new.user@test.com';
+    const password = 'test-password';
+    user.password = await Auth.encryptPassword(password);
+    user.role = UserRole.Admin;
+
+    const entityManager = getManager();
+    await entityManager.save(user);
+
+    const res = await request(app).post('/auth/login').send({
+      email: user.email,
+      password,
+    });
+
+    expect(res.status).toEqual(200);
     expect(typeof res.body.token).toBe('string');
     expect(typeof res.body.expiresIn).toBe('string');
     done();
   });
 
-  it('POST /login should return 400 Bad Request with an error message when given invalid credentials', async (done) => {
+  it('POST /login should return 400 Bad Request when given invalid credentials', async (done) => {
     const res = await request(app).post('/auth/login').send({
-      email: 'invalid-email-test',
-      password: 'invalid-password-test',
+      email: 'random.test@mail.com',
+      password: 'test-random-pass',
     });
+
     expect(res.status).toEqual(400);
-    expect(typeof res.body.message).toBe('string');
     done();
   });
 });
