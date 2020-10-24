84919f431cb768b01a9108d1f7a2dd35bffa5804
Samy Ouyahia <ouyahia.samy@yahoo.fr>
2020-10-24 15:54:49 +0200

    Small formatting

--- test/testHelpers/database.helper.ts
+++ test/testHelpers/database.helper.ts
@@ -3,12 +3,16 @@ import { Connection } from 'typeorm';
 export async function clearDB(connection: Connection): Promise<void> {
   let repo = connection.getRepository('Keywords');
   await repo.query('DELETE FROM Keywords;');
+
   repo = connection.getRepository('TransactionCategories');
   await repo.query('DELETE FROM TransactionCategories;');
+
   repo = connection.getRepository('Categories');
   await repo.query('DELETE FROM Categories;');
+
   repo = connection.getRepository('Transactions');
   await repo.query('DELETE FROM Transactions;');
+
   repo = connection.getRepository('Users');
   await repo.query('DELETE FROM Users;');
 }
